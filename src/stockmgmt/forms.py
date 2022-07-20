5d60c8e9a98da7e5410281f6530762d3a95533ba
eminKolasinac <emin_kolasinac@outlook.com>
2022-07-20 23:24:54 +0200

    Setup app frontend, added some pages: home, add item, list item,added redirect after add item to prevent double adds

--- src/stockmgmt/forms.py
+++ src/stockmgmt/forms.py
@@ -1,7 +1,9 @@
+from random import randint, random
 from django import forms
 from .models import Stock
+import random
 
 class StockCreateForm(forms.ModelForm):
     class Meta:
         model = Stock
-        fields = ['category','item_name', 'quantity']
+        fields = ["id",'category','item_name', 'quantity']
--- src/stockmgmt/views.py
+++ src/stockmgmt/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from .models import *
+from .forms import StockCreateForm
 
 # Create your views here.
+def home(request):
+    title = "Welcome to your Home Page"
+    context = {
+        "title": title,   #varijabila koju pozivamo u html fajlu
+    }
+    return render(request, "home.html",context)  #renderovanje
+
+def list_items(request):
+    title = "List of list_items"
+    queryset = Stock.objects.all()
+    context = {
+        "title": title,   #varijabila koju pozivamo u html fajlu
+        "queryset": queryset,
+    }
+    return render(request, "list_items.html",context)  #renderovanje
+
+def add_items(request):
+    form = StockCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect(list_items)
+    context = {
+        "form": form,
+        "title": "Add Item",
+    }
+    return render(request, "add_items.html", context)
